40086e2fccb8701c3098a11cae55659e6881eeea
eksdeemephisto <shuyinvenus@gmail.com>
2023-06-15 18:40:17 -0400

    update 6/15/2023 1840

---
+++ src/test/java/class07/Tables.java
@@ -0,0 +1,48 @@
+package class07;
+
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Tables extends CommonMethods {
+    public static void main(String[] args) {
+
+        String url = "https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+//        print the whole table on the console
+
+//***************table level access*********************
+//        get the whole table
+        WebElement wholeTable = driver.findElement(By.xpath("//table[@id='customers']"));
+        String allText = wholeTable.getText();
+        System.out.println(allText);
+
+        System.out.println("********************************************************");
+//****************row level access of table*************
+//        print all the rows of the table
+//modified: print the row that contains google as company name
+        List<WebElement> allRows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+        for (WebElement row : allRows) {
+            String rowText = row.getText();
+            if (rowText.contains("Google")) {
+                System.out.println(rowText);
+            }
+
+        }
+
+
+        System.out.println("--------------------------------------------------------");
+//*****************column level access************************
+//        print each and every data in the table
+
+        List<WebElement> allCellData = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr/td"));
+        for (WebElement cell : allCellData) {
+            String cellData = cell.getText();
+            System.out.println(cellData);
+        }
+
+    }
+}
